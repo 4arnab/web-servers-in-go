2399e3fc6388b299d4efad2e777cb65c24de67c5
ahmed <axmedarna3@gmail.com>
2023-05-18 15:24:31 +0300

    geting started and serving simple web app

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world, web servers in golang")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, error := fmt.Fprintf(w, "hello, from the server side")
+
+		if error != nil {
+			fmt.Println(error.Error())
+			panic(error)
+		}
+
+		fmt.Println(n)
+
+	})
+
+	fmt.Print("Listening on http://localhost:4000... ")
+	http.ListenAndServe(":4000", nil)
 }
